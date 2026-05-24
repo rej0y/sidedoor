13ac4e316a3a8024fa7ab2f8b0a5df3b5f8e3f9f
Mattcraft900 <mattcraft900@gmail.com>
2026-05-24 17:54:10 -0600

    rough beginnings of JS professor card creation

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+import { buildProfessorList} from './prof-list.js';
+
+/* ----- Sample API request (temp/one-time) ----- */
+// Send a fetch request to the API
+const response = await fetch(/* "https://whatever-the-api-url-is.com" */);
+// It returns a huge string, which needs to be parsed as JSON data
+const professors = await response.json();
+
+// Build out the appropriate DOM elements using the API fetch results
+buildProfessorList(professors);
+
---
+++ scripts/prof-list.js
@@ -0,0 +1,38 @@
+
+const profList = document.getElementById('prof-list');
+
+
+// Creates DOM elements for each prof in the JSON list
+// It currently does this in the same order the objects are given
+export function buildProfessorList(professors) {
+    // For each professor (which we name "prof") in the professor list:
+    professors.forEach(prof => {
+        buildProfessorCard(prof);
+    })
+}
+
+// *** It's VERY POSSIBLE that the JSON property names will have to changed to match the actual ones!!
+function buildProfessorCard(prof) {
+    // Create a new blank div element to work with
+    const newCard = document.createElement("div");
+    // Give it the desired HTML classes
+    newCard.className = "card professor-card";
+
+    // Store useful data from the professor JSON object as HTML dataset attributes
+    // Most of the data will be stored in the DOM somewhere already as there own elements, generated below
+    newCard.dataset.id = prof.id;
+    newCard.dataset.dept = prof.department; // ("department" just makes more sense than "college" tbh)
+
+    // Put the actual HTML content we want into the newCard element
+    // *** THIS IS VERY INCOMPLETE but functional, it will need a lot of expanding/polishing
+    newCard.innerHTML = `
+        <span class="prof-name">${prof.name}</span>
+        <span class="prof-spec">${prof.specialization}</span>
+        <span class="prof-loc">${prof.building} ${prof.roomNumber}</span>
+    `
+    // Finally, attach the newCard div to the DOM
+    profList.appendChild(newCard);
+}
+
+
+
