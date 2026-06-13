b6edc468d8d2e4928778a7922c5d38ae7a9a91dd
Mattcraft900 <mattcraft900@gmail.com>
2026-06-12 18:12:49 -0600

    Page generates ALL professors dynamically from database

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
 
 import { buildProfessorList } from './prof-list.js';
 
-/*
+
 // ----- Sample API request (temp/one-time) ----- //
 // Send a fetch request to the API
-const response = await fetch("https://whatever-the-api-url-is.com");
+const response = await fetch("http://66.112.209.106:3000/professors");
 // It returns a huge string, which needs to be parsed as JSON data
 const professors = await response.json();
-*/
 
-// --- Temporarily using this mock API response instead --- //
-import professors from './mock_api_response.js';
+
+// --- Hard-coded mock API response for testing purposes --- //
+// import professors from './mock_api_response.js';
 
 
 // Build out the appropriate DOM elements using the API fetch results
