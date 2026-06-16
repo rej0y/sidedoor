a5a754e9a7e5ab4fb77d9516eafbc8d385d77fba
rej0y <zhoushengjian1@gmail.com>
2026-06-16 14:27:17 -0600

    Use HTTPS API endpoint (#11)

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,12 @@
-
+import { API_BASE_URL } from './config.js';
 import { buildProfessorList } from './prof-list.js';
 
+const response = await fetch(`${API_BASE_URL}/professors`);
 
-// ----- Sample API request (temp/one-time) ----- //
-// Send a fetch request to the API
-const response = await fetch("http://66.112.209.106:3000/professors");
-// It returns a huge string, which needs to be parsed as JSON data
+if (!response.ok) {
+    throw new Error(`Failed to fetch professors: ${response.status}`);
+}
+
 const professors = await response.json();
 
-
-// --- Hard-coded mock API response for testing purposes --- //
-// import professors from './mock_api_response.js';
-
-
-// Build out the appropriate DOM elements using the API fetch results
 buildProfessorList(professors);
